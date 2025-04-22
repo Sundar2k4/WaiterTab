3c4fe255ac76422d5ce4b7319043afc98bb29e4a
Sundar.c <csundar2004@gmail.com>
2025-04-22 17:08:25 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb+srv://csundar993:S1RjXYDtC73UGJCE@cluster2.3g8fa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+  
 
 app.get('/',(req,res)=>
 {
@@ -59,6 +65,5 @@ app.delete("/dish-delete/:id",async (req,res)=>{
     }
 });
 
-app.listen(5000,()=>{
-    console.log('connected to port 5000')
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
